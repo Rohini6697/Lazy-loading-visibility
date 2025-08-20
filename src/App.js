5552722f2850dd57abdff544986f78914f0e8e9d
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-08-20 09:30:22 +0530

    import on visibility

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState,useRef,useEffect } from 'react';
+
+function App() {
+  const [component,setComponent] = useState(null)
+  const [observer,setObserver] = useState(null)
+  const elementRef = useRef(null)
+
+  useEffect(() =>{
+    if (!observer){
+      const newObserver = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          import('./component/LazyComponent').then((mod) => {
+            setComponent(mod.default);
+          });
+          newObserver.disconnect();
+        }
+      })
+      setObserver(newObserver);
+    }
+    if (observer && elementRef.current) {
+      observer.observe(elementRef.current)
+    }
+
+    return () => {
+      if (observer) {
+        observer.disconnect();
+      }
+    }
+  }, [observer])
+  if(!component)
+    return (<>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <div ref = {elementRef}>Loading.....</div>
+  </>)
+  return component
+}
+
+export default App;
---
+++ src/component/LazyComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LazyComponent = () => {
+  return (
+    <div>
+      <h1>Its lazy component</h1>
+    </div>
+  )
+}
+
+export default LazyComponent
